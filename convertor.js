fed917402aeeafd7684e9527f24c8909f43b82a0
Khushi-0077 <138706118+Khushi-0077@users.noreply.github.com>
2023-08-02 12:04:49 -0400

    Create convertor.js

---
+++ convertor.js
@@ -0,0 +1,20 @@
+Khushpreet Kaur
+c0886593
+26 july
+
+$(document).ready(function() {
+
+    $('#convert').click(function() {
+      var btc = $('#btc').val();
+      $.ajax({
+        url: 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json',
+        success: function(data) {
+          var rate = JSON.parse(data).bpi.USD.rate_float;
+          var usd = btc * rate;
+          $('#result').text(btc + ' Bitcoin equals ' + usd + ' USD');
+        }
+      });
+  
+    });
+  
+  });
